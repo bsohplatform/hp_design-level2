8395af138ebf4196c90c5e23a6c80609cfcf08bb
bsohplatform <bongseongoh@kier.re.kr>
2022-07-08 10:36:49 +0900

    commit 7 condensor
    - 외부 인스턴스로 사용되지 않는 변수에 self. 모조리 삭제

--- condensor.py
+++ condensor.py
@@ -21,102 +21,167 @@ class Condensor_module:
         
     def Condensor_FTHE(self):
         
-        self.x_cond_ref = np.ones(shape=(self.setting.cond_N_element+1, self.setting.cond_N_row+1))
-        self.h_cond_ref = np.zeros(shape=(self.setting.cond_N_element+1, self.setting.cond_N_row+1))
-        self.T_cond_ref = copy.deepcopy(self.h_cond_ref)
-        self.P_cond_ref = copy.deepcopy(self.h_cond_ref)
-        self.T_cond_sec = copy.deepcopy(self.h_cond_ref)
-        self.P_cond_sec = copy.deepcopy(self.h_cond_ref)
-        self.h_cond_sec = copy.deepcopy(self.h_cond_ref)
-        self.LMTD_cond = np.zeros(shape=(self.setting.cond_N_element, self.setting.cond_N_row))
-        self.cond_UA_cond_element = copy.deepcopy(self.LMTD_cond)
+        x_cond_ref = np.ones(shape=(self.setting.cond_N_element+1, self.setting.cond_N_row+1))
+        h_cond_ref = np.zeros(shape=(self.setting.cond_N_element+1, self.setting.cond_N_row+1))
+        T_cond_ref = copy.deepcopy(h_cond_ref)
+        P_cond_ref = copy.deepcopy(h_cond_ref)
+        T_cond_sec = copy.deepcopy(h_cond_ref)
+        P_cond_sec = copy.deepcopy(h_cond_ref)
+        h_cond_sec = copy.deepcopy(h_cond_ref)
+        LMTD_cond = np.zeros(shape=(self.setting.cond_N_element, setting.cond_N_row))
+        cond_UA_cond_element = copy.deepcopy(LMTD_cond)
         
-        self.P_cond_sec[:,0] = self.second_out.p
-        self.T_cond_sec[:,0] = self.second_out.T
-        self.h_cond_sec[:,0] = self.second_out.h
+        P_cond_sec[:,0] = self.second_out.p
+        T_cond_sec[:,0] = self.second_out.T
+        h_cond_sec[:,0] = self.second_out.h
         
-        self.P_cond_ref[0,0] = self.primary_out.p
-        self.T_cond_ref[0,0] = self.primary_out.T
-        self.h_cond_ref[0,0] = self.primary_out.h
+        P_cond_ref[0,0] = self.primary_out.p
+        T_cond_ref[0,0] = self.primary_out.T
+        h_cond_ref[0,0] = self.primary_out.h
         
-        if self.P_cond_ref[0,0] < self.primary_in.p_crit:
-            self.h_cond_refl = PropsSI('H','P',self.P_cond_ref[0,0],'Q',0.0,self.primary_in.fluidmixture)
-            self.h_cond_refg = PropsSI('H','P',self.P_cond_ref[0,0],'Q',1.0,self.primary_in.fluidmixture)
-            self.x_cond_ref[0,0] = (self.h_cond_ref[0,0]-self.h_cond_refl)/(self.h_cond_refg-self.h_cond_refl)
+        if P_cond_ref[0,0] < self.primary_in.p_crit:
+            h_cond_refl = PropsSI('H','P',P_cond_ref[0,0],'Q',0.0,self.primary_in.fluidmixture)
+            h_cond_refg = PropsSI('H','P',P_cond_ref[0,0],'Q',1.0,self.primary_in.fluidmixture)
+            x_cond_ref[0,0] = (h_cond_ref[0,0]-h_cond_refl)/(h_cond_refg-h_cond_refl)
         
         for j in range(self.setting.cond_N_row):
             for jj in range(self.setting.cond_N_element):
                 if round(j/2) == j/2: # 짝수
-                    self.h_cond_ref[jj+1,j] = self.h_cond_ref[jj,j] - (self.primary_in.q/self.setting.cond_N_element/self.setting.cond_N_row)/self.primary_in.m
-                    self.P_cond_ref[jj+1,j] = self.P_cond_ref[jj,j] - (self.primary_in.p - self.primary_out.p)/self.setting.cond_N_element/self.setting.cond_N_row
-                    self.T_cond_ref[jj+1,j] = PropsSI('T','P',self.P_cond_ref[jj+1,j],'H',self.h_cond_ref[jj+1,j],self.primary_in.fluidmixture)
+                    h_cond_ref[jj+1,j] = h_cond_ref[jj,j] - (self.primary_in.q/self.setting.cond_N_element/self.setting.cond_N_row)/self.primary_in.m
+                    P_cond_ref[jj+1,j] = P_cond_ref[jj,j] - (self.primary_in.p - self.primary_out.p)/self.setting.cond_N_element/self.setting.cond_N_row
+                    T_cond_ref[jj+1,j] = PropsSI('T','P',P_cond_ref[jj+1,j],'H',h_cond_ref[jj+1,j],self.primary_in.fluidmixture)
                     
-                    if self.P_cond_ref[jj+1,j] < self.primary_in.p_crit:
-                        self.h_cond_refl = PropsSI('H','P',self.P_cond_ref[jj+1,j],'Q',0.0,self.primary_in.fluidmixture)
-                        self.h_cond_refg = PropsSI('H','P',self.P_cond_ref[jj+1,j],'Q',1.0,self.primary_in.fluidmixture)
-                        self.x_cond_ref[0,0] = (self.h_cond_ref[jj+1,j]-self.h_cond_refl)/(self.h_cond_refg-self.h_cond_refl)
+                    if P_cond_ref[jj+1,j] < self.primary_in.p_crit:
+                        h_cond_refl = PropsSI('H','P',P_cond_ref[jj+1,j],'Q',0.0,self.primary_in.fluidmixture)
+                        h_cond_refg = PropsSI('H','P',P_cond_ref[jj+1,j],'Q',1.0,self.primary_in.fluidmixture)
+                        x_cond_ref[0,0] = (h_cond_ref[jj+1,j]-h_cond_refl)/(h_cond_refg-h_cond_refl)
                         
-                    self.h_cond_sec[jj,j+1] = self.h_cond_sec[jj,j] - self.primary_in.q/self.setting.cond_N_row/self.second_in.m
-                    self.P_cond_sec[jj,j+1] = self.P_cond_sec[jj,j] + (self.second_in.p - self.second_out.p)/self.setting.cond_N_row
-                    self.T_cond_sec[jj,j+1] = PropsSI('T','H',self.h_cond_sec[jj,j+1],'P',self.P_cond_sec[jj,j+1],self.second_in.fluidmixture)
+                    h_cond_sec[jj,j+1] = h_cond_sec[jj,j] - self.primary_in.q/self.setting.cond_N_row/self.second_in.m
+                    P_cond_sec[jj,j+1] = P_cond_sec[jj,j] + (self.second_in.p - self.second_out.p)/self.setting.cond_N_row
+                    T_cond_sec[jj,j+1] = PropsSI('T','H',h_cond_sec[jj,j+1],'P',P_cond_sec[jj,j+1],self.second_in.fluidmixture)
                     
-                    if self.T_cond_ref[jj+1,j] - self.T_cond_sec[jj,j+1] < 0:
+                    if T_cond_ref[jj+1,j] - T_cond_sec[jj,j+1] < 0:
                         self.errmsg.cond_T_rvs = 1
                         break
                     else:
                         self.errmsg.cond_T_rvs = 0
-                        self.LMTD_cond[jj,j] = ((self.T_cond_ref[jj+1,j]-self.T_cond_sec[jj,j+1]) - (self.T_cond_ref[jj,j]-self.T_cond_sec[jj,j]))\
-                            /np.log((self.T_cond_ref[jj+1,j]-self.T_cond_sec[jj,j+1]) - (self.T_cond_ref[jj,j]-self.T_cond_sec[jj,j]))
-                        self.cond_UA_cond_element[jj,j] = (self.primary_in.q/self.setting.cond_N_element/self.setting.cond_N_row) / self.LMTD_cond[jj,j]
+                        LMTD_cond[jj,j] = ((T_cond_ref[jj+1,j]-T_cond_sec[jj,j+1]) - (T_cond_ref[jj,j]-T_cond_sec[jj,j]))\
+                            /np.log((T_cond_ref[jj+1,j]-T_cond_sec[jj,j+1]) - (T_cond_ref[jj,j]-T_cond_sec[jj,j]))
+                        cond_UA_cond_element[jj,j] = (self.primary_in.q/self.setting.cond_N_element/self.setting.cond_N_row) / LMTD_cond[jj,j]
                         
                         if jj == self.setting.cond_N_element-1:
-                            self.h_cond_ref[self.setting.cond_N_element,j+1] = self.h_cond_ref[self.setting.cond_N_element,j]
-                            self.T_cond_ref[self.setting.cond_N_element,j+1] = self.T_cond_ref[self.setting.cond_N_element,j]
-                            self.P_cond_ref[self.setting.cond_N_element,j+1] = self.P_cond_ref[self.setting.cond_N_element,j]
-                            self.x_cond_ref[self.setting.cond_N_element,j+1] = self.x_cond_ref[self.setting.cond_N_element,j]
+                            h_cond_ref[self.setting.cond_N_element,j+1] = h_cond_ref[self.setting.cond_N_element,j]
+                            T_cond_ref[self.setting.cond_N_element,j+1] = T_cond_ref[self.setting.cond_N_element,j]
+                            P_cond_ref[self.setting.cond_N_element,j+1] = P_cond_ref[self.setting.cond_N_element,j]
+                            x_cond_ref[self.setting.cond_N_element,j+1] = x_cond_ref[self.setting.cond_N_element,j]
                             
                 else: # 홀수
-                    self.h_cond_ref[self.setting.cond_N_element-jj-1,j] = self.h_cond_ref[self.setting.cond_N_element-jj,j] - (self.primary_in.q/self.setting.cond_N_element/self.setting.cond_N_row)/self.primary_in.m
-                    self.P_cond_ref[self.setting.cond_N_element-jj-1,j] = self.P_cond_ref[self.setting.cond_N_element-jj,j] - (self.primary_in.p - self.primary_out.p)/self.setting.cond_N_element/self.setting.cond_N_row
-                    self.T_cond_ref[self.setting.cond_N_element-jj-1,j] = PropsSI('T','P',self.P_cond_ref[self.setting.cond_N_element-jj-1,j],'H',self.h_cond_ref[self.setting.cond_N_element-jj-1,j],self.primary_in.fluidmixture)
+                    h_cond_ref[self.setting.cond_N_element-jj-1,j] = h_cond_ref[self.setting.cond_N_element-jj,j] - (self.primary_in.q/self.setting.cond_N_element/self.setting.cond_N_row)/self.primary_in.m
+                    P_cond_ref[self.setting.cond_N_element-jj-1,j] = P_cond_ref[self.setting.cond_N_element-jj,j] - (self.primary_in.p - self.primary_out.p)/self.setting.cond_N_element/self.setting.cond_N_row
+                    T_cond_ref[self.setting.cond_N_element-jj-1,j] = PropsSI('T','P',P_cond_ref[self.setting.cond_N_element-jj-1,j],'H',h_cond_ref[self.setting.cond_N_element-jj-1,j],self.primary_in.fluidmixture)
                     
-                    if self.P_cond_ref[self.setting.cond_N_element-jj-1,j] < self.primary_in.p_crit:
-                        self.h_cond_refl = PropsSI('H','P',self.P_cond_ref[self.setting.cond_N_element-jj-1,j],'Q',0.0,self.primary_in.fluidmixture)
-                        self.h_cond_refg = PropsSI('H','P',self.P_cond_ref[self.setting.cond_N_element-jj-1,j],'Q',1.0,self.primary_in.fluidmixture)
-                        self.x_cond_ref[self.setting.cond_N_element-jj-1,j] = (self.h_cond_ref[self.setting.cond_N_element-jj-1,j]-self.h_cond_refl)/(self.h_cond_refg-self.h_cond_refl)
+                    if P_cond_ref[self.setting.cond_N_element-jj-1,j] < self.primary_in.p_crit:
+                        h_cond_refl = PropsSI('H','P',P_cond_ref[self.setting.cond_N_element-jj-1,j],'Q',0.0,self.primary_in.fluidmixture)
+                        h_cond_refg = PropsSI('H','P',P_cond_ref[self.setting.cond_N_element-jj-1,j],'Q',1.0,self.primary_in.fluidmixture)
+                        x_cond_ref[self.setting.cond_N_element-jj-1,j] = (h_cond_ref[self.setting.cond_N_element-jj-1,j]-h_cond_refl)/(h_cond_refg-h_cond_refl)
                         
-                    self.h_cond_sec[self.setting.cond_N_element-jj-1,j+1] = self.h_cond_sec[self.setting.cond_N_element-jj-1,j] - self.primary_in.q/self.setting.cond_N_row/self.second_in.m
-                    self.P_cond_sec[self.setting.cond_N_element-jj-1,j+1] = self.P_cond_sec[self.setting.cond_N_element-jj-1,j] + (self.second_in.p - self.second_in.p)/self.setting.cond_N_row
-                    self.T_cond_sec[self.setting.cond_N_element-jj-1,j+1] = PropsSI('T','H',self.h_cond_sec[self.setting.cond_N_element-jj-1,j+1],'P',self.P_cond_sec[self.setting.cond_N_element-jj-1,j+1],self.second_in.fluidmixture)
+                    h_cond_sec[self.setting.cond_N_element-jj-1,j+1] = h_cond_sec[self.setting.cond_N_element-jj-1,j] - self.primary_in.q/self.setting.cond_N_row/self.second_in.m
+                    P_cond_sec[self.setting.cond_N_element-jj-1,j+1] = P_cond_sec[self.setting.cond_N_element-jj-1,j] + (self.second_in.p - self.second_in.p)/self.setting.cond_N_row
+                    T_cond_sec[self.setting.cond_N_element-jj-1,j+1] = PropsSI('T','H',h_cond_sec[self.setting.cond_N_element-jj-1,j+1],'P',P_cond_sec[self.setting.cond_N_element-jj-1,j+1],self.second_in.fluidmixture)
                     
-                    if self.T_cond_ref[self.setting.cond_N_element-jj-1,j] - self.T_cond_sec[self.setting.cond_N_element-jj-1,j+1] < 0:
+                    if T_cond_ref[self.setting.cond_N_element-jj-1,j] - T_cond_sec[self.setting.cond_N_element-jj-1,j+1] < 0:
                         self.errmsg.cond_T_rvs = 1
                         break
                     else:
                         self.errmsg.cond_T_rvs = 0
-                        self.LMTD_cond[self.setting.cond_N_element-jj-1,j] = ((self.T_cond_ref[self.setting.cond_N_element-jj-1,j]-self.T_cond_sec[self.setting.cond_N_element-jj-1,j+1]) - (self.T_cond_ref[self.setting.cond_N_element-jj,j]-self.T_cond_sec[self.setting.cond_N_element-jj-1,j]))\
-                            /np.log((self.T_cond_ref[self.setting.cond_N_element-jj-1,j]-self.T_cond_sec[self.setting.cond_N_element-jj-1,j+1]) - (self.T_cond_ref[self.setting.cond_N_element-jj,j]-self.T_cond_sec[self.setting.cond_N_element-jj-1,j]))
-                        self.cond_UA_cond_element[self.setting.cond_N_element-jj-1,j] = (self.primary_in.q/self.setting.cond_N_element/self.setting.cond_N_row) / self.LMTD_cond[self.setting.cond_N_element-jj-1,j]
+                        LMTD_cond[self.setting.cond_N_element-jj-1,j] = ((T_cond_ref[self.setting.cond_N_element-jj-1,j]-T_cond_sec[self.setting.cond_N_element-jj-1,j+1]) - (T_cond_ref[self.setting.cond_N_element-jj,j]-T_cond_sec[self.setting.cond_N_element-jj-1,j]))\
+                            /np.log((T_cond_ref[self.setting.cond_N_element-jj-1,j]-T_cond_sec[self.setting.cond_N_element-jj-1,j+1]) - (T_cond_ref[self.setting.cond_N_element-jj,j]-T_cond_sec[self.setting.cond_N_element-jj-1,j]))
+                        cond_UA_cond_element[self.setting.cond_N_element-jj-1,j] = (self.primary_in.q/self.setting.cond_N_element/self.setting.cond_N_row) / LMTD_cond[self.setting.cond_N_element-jj-1,j]
                         
                         if jj == self.setting.cond_N_element-1:
-                            self.h_cond_ref[0,j+1] = self.h_cond_ref[0,j]
-                            self.T_cond_ref[0,j+1] = self.T_cond_ref[0,j]
-                            self.P_cond_ref[0,j+1] = self.P_cond_ref[0,j]
-                            self.x_cond_ref[0,j+1] = self.x_cond_ref[0,j]
+                            h_cond_ref[0,j+1] = h_cond_ref[0,j]
+                            T_cond_ref[0,j+1] = T_cond_ref[0,j]
+                            P_cond_ref[0,j+1] = P_cond_ref[0,j]
+                            x_cond_ref[0,j+1] = x_cond_ref[0,j]
                 
                 if self.errmsg.cond_T_rvs == 1:
                     break
-        self.setting.cond_UA = np.sum(np.sum(self.cond_UA_cond_element))
-        self.cond_T_lm = self.primary_in.q/self.setting.cond_UA
+        self.setting.cond_UA = np.sum(np.sum(cond_UA_cond_element))
+        self.setting.cond_T_lm = self.primary_in.q/self.setting.cond_UA
         if np.round(self.setting.cond_N_row/2) != self.setting.cond_N_row/2: # 짝수
-            self.primary_out.T = self.T_cond_ref[self.setting.cond_N_element,self.setting.cond_N_row-1]
-            self.primary_out.h = self.h_cond_ref[self.setting.cond_N_element,self.setting.cond_N_row-1]
-            self.primary_out.x = self.x_cond_ref[self.setting.cond_N_element,self.setting.cond_N_row-1]
+            self.primary_out.T = T_cond_ref[self.setting.cond_N_element,self.setting.cond_N_row-1]
+            self.primary_out.h = h_cond_ref[self.setting.cond_N_element,self.setting.cond_N_row-1]
+            self.primary_out.x = x_cond_ref[self.setting.cond_N_element,self.setting.cond_N_row-1]
         else: # 홀수
-            self.primary_out.T = self.T_cond_ref[0,self.setting.cond_N_row-1]
-            self.primary_out.h = self.h_cond_ref[0,self.setting.cond_N_row-1]
-            self.primary_out.x = self.x_cond_ref[0,self.setting.cond_N_row-1]
-
+            self.primary_out.T = T_cond_ref[0,self.setting.cond_N_row-1]
+            self.primary_out.h = h_cond_ref[0,self.setting.cond_N_row-1]
+            self.primary_out.x = x_cond_ref[0,self.setting.cond_N_row-1]
+            
+            
+    def Condensor_PHE(self):
+        x_cond_ref = np.ones(shape=(self.setting.cond_N_element+1))
+        h_cond_ref = np.zeros(shape=(self.setting.cond_N_element+1))
+        T_cond_ref = copy.deepcopy(h_cond_ref)
+        P_cond_ref = copy.deepcopy(h_cond_ref)
+        T_cond_sec = copy.deepcopy(h_cond_ref)
+        P_cond_sec = copy.deepcopy(h_cond_ref)
+        h_cond_sec = copy.deepcopy(h_cond_ref)
+        dT_cond = copy.deepcopy(h_cond_ref)
+        LMTD_cond = np.zeros(shape=(self.setting.cond_N_element))
+        cond_UA_cond_element = copy.deepcopy(LMTD_cond)
+        
+        P_cond_sec[0] = self.second_out.p
+        T_cond_sec[0] = self.second_out.T
+        h_cond_sec[0] = self.second_out.h
+        
+        P_cond_ref[0] = self.primary_in.p
+        T_cond_ref[0] = self.primary_in.T
+        h_cond_ref[0] = self.primary_in.h
+        
+        if P_cond_ref[0] < self.primary_in.p_crit:
+            h_cond_refl = PropsSI('H','P',P_cond_ref[0],'Q',0.0,self.primary_in.fluidmixture)
+            h_cond_refg = PropsSI('H','P',P_cond_ref[0],'Q',1.0,self.primary_in.fluidmixture)
+            x_cond_ref[0] = (h_cond_ref[0]-h_cond_refl)/(h_cond_refg-h_cond_refl)
+        
+        dT_cond[0] = T_cond_ref[0] - T_cond_sec[0]
+        
+        if dT_cond[0] < 0.0:
+            self.errmsg.cond_T_rvs = 1
+        else:
+            self.errmsg.cond_T_rvs = 0
+    
+        for i in range(self.setting.cond_N_element):
+            h_cond_ref[i+1] = h_cond_ref[i] - self.primary_in.q/self.setting.cond_N_element/self.primary_in.m
+            P_cond_ref[i+1] = P_cond_ref[i] - (self.primary_in.p - self.primary_out.p)/self.setting.cond_N_element
+            T_cond_ref[i+1] = PropsSI('T','H',h_cond_ref[i+1],'P',P_cond_ref[i+1],self.primary_in.fluidmixture)
+            
+            if P_cond_ref[i+1] < self.primary_in.p_crit:
+                h_cond_refl = PropsSI('H','P',P_cond_ref[i+1],'Q',0.0,self.primary_in.fluidmixture)
+                h_cond_refg = PropsSI('H','P',P_cond_ref[i+1],'Q',1.0,self.primary_in.fluidmixture)
+                x_cond_ref[i+1] = (h_cond_ref[i+1]-h_cond_refl)/(h_cond_refg-h_cond_refl)
+            
+            h_cond_sec[i+1] = h_cond_sec[i] - self.primary_in.q/self.setting.cond_N_row/self.second_in.m
+            P_cond_sec[i+1] = P_cond_sec[i] + (self.second_in.p - self.second_out.p)/self.setting.cond_N_row
+            T_cond_sec[i+1] = PropsSI('T','H',h_cond_sec[i+1],'P',P_cond_sec[i+1],self.second_in.fluidmixture)
+            
+            dT_cond[i+1] = T_cond_ref[i+1] - T_cond_sec[i+1]
+            if dT_cond[i+1] < 0.0:
+                self.errmsg.cond_T_rvs = 1
+                break
+            else:
+                self.errmsg.cond_T_rvs = 0
+                LMTD_cond[i] = ((T_cond_ref[i+1]-T_cond_sec[i+1]) - (T_cond_ref[i]-T_cond_sec[i]))\
+                            /np.log((T_cond_ref[i+1]-T_cond_sec[i+1]) - (T_cond_ref[i]-T_cond_sec[i]))
+                            
+                cond_UA_cond_element[i] = (self.primary_in.q/self.setting.cond_N_element/self.setting.cond_N_row) / LMTD_cond[i]    
+        
+        self.setting.cond_UA = np.sum(cond_UA_cond_element)
+        self.setting.cond_T_pp = min(dT_cond)
+        self.primary_out.T = T_cond_ref[self.setting.cond_N_element]
+        self.primary_out.h = h_cond_ref[self.setting.cond_N_element]
+        self.primary_out.x = x_cond_ref[self.setting.cond_N_element]
+        
+            
 
 if __name__ == "__main__":
         
@@ -145,8 +210,6 @@ if __name__ == "__main__":
         setting = Settings(cond_N_row = 3)
         errmsg = Errormsg()
         cond_basic = Condensor_module(primary_in, primary_out, second_in, second_out, setting, errmsg)
-        cond_basic.Condensor_FTHE()
+        cond_basic.Condensor_PHE()
         
-        print(cond_basic.h_cond_ref)
-        print(cond_basic.T_cond_ref)
-        
+        print(primary_out.h)
