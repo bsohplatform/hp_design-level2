1c6e111ec32b38c5d37b949fa6b32e1701bab9e7
bsohplatform <bongseongoh@kier.re.kr>
2022-08-09 14:39:54 +0900

    commit. COMPAND_module
    - 출구 s 계산 로직 추가

--- COMPAND_module.py
+++ COMPAND_module.py
@@ -11,6 +11,7 @@ class Compander_module:
             h_comp_out_isen = PropsSI('H','P',self.primary_out.p,'S',self.primary_in.s,self.primary_in.fluidmixture)
             self.primary_out.h = (h_comp_out_isen - self.primary_in.h)/eff_isen + self.primary_in.h
             self.primary_out.T = PropsSI('T','H',self.primary_out.h,'P',self.primary_out.p,self.primary_in.fluidmixture)
+            self.primary_out.s = PropsSI('S','T',self.primary_out.T,'P',self.primary_out.p,self.primary_in.fluidmixture)
             try:
                 T_comp_out_sat = PropsSI('T','P',self.primary_out.p,'Q',1.0,self.primary_in.fluidmixture)
             except:
@@ -26,4 +27,18 @@ class Compander_module:
         self.primary_out.h = self.primary_in.h - (self.primary_in.h - h_expand_out_isen)*eff_isen
         self.primary_out.T = PropsSI('T','H',self.primary_out.h,'P',self.primary_out.p,self.primary_in.fluidmixture)
         
+        try:
+            T_expand_out_sat = PropsSI('T','P',self.primary_out.p,'Q',1.0,self.primary_in.fluidmixture)
+        except:
+            T_expand_out_sat = self.primary_out.T_crit
+        
+        if eff_isen > 0.0:
+            if self.primary_out < T_expand_out_sat:
+                primary_out_hg = PropsSI('H','P',self.primary_out.p,'Q',1.0, self.primary_in.fluidmixture)
+                primary_out_hl = PropsSI('H','P',self.primary_out.p,'Q',0.0, self.primary_in.fluidmixture)
+                primary_out_x = (self.primary_out.h - primary_out_hl)/(primary_out_hg - primary_out_hl)
+                primary_out_sg = PropsSI('S','P',self.primary_out.p,'Q',1.0, self.primary_in.fluidmixture)
+                primary_out_sl = PropsSI('S','P',self.primary_out.p,'Q',0.0, self.primary_in.fluidmixture)
+                self.primary_out.s = primary_out_sg*primary_out_x + primary_out_sl*(1-primary_out_x)
+                
         self.Pspecific = (self.primary_in.h - self.primary_out.h)*eff_mech
