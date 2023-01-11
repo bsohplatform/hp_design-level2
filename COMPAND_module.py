a69cd7e10e1611c3fbe94d189a9bf17e175ae80d
bsohplatform <bongseongoh@kier.re.kr>
2023-01-11 15:16:15 +0900

    commit. Overhang 문제 해결 커밋 기존. While 문으로 압축기 입구 온도 1도 올리는 것으로 반복 문제. 압축기 입구 온도가 반영이 안되서 무한루프
    해결. 압축기 입구 온도를 올리는 것이 아니라 과열도를 올리도록 알고리즘 수정
    오버행 문제일 때 cond_a = 0 으로 설정하여 Highpressure solver 중지되고 바로 Cycle_solver로 넘어가도록 수정

--- COMPAND_module.py
+++ COMPAND_module.py
@@ -5,25 +5,27 @@ class Compander_module:
     def __init__(self, primary_in, primary_out):
         self.primary_in = primary_in
         self.primary_out = primary_out
-    
-    def COMP(self, eff_isen: float, eff_mech: float):    
-        while 1:
-            h_comp_out_isen = PropsSI('H','P',self.primary_out.p,'S',self.primary_in.s,self.primary_in.fluidmixture)
-            self.primary_out.h = (h_comp_out_isen - self.primary_in.h)/eff_isen + self.primary_in.h
-            self.primary_out.T = PropsSI('T','H',self.primary_out.h,'P',self.primary_out.p,self.primary_in.fluidmixture)
-            self.primary_out.s = PropsSI('S','T',self.primary_out.T,'P',self.primary_out.p,self.primary_in.fluidmixture)
-            try:
-                T_comp_out_sat = PropsSI('T','P',self.primary_out.p,'Q',1.0,self.primary_in.fluidmixture)
-            except:
-                T_comp_out_sat = self.primary_out.T_crit
-            if (self.primary_out.T - T_comp_out_sat) > 0.01: # Overhanging problem protection
-                break
-            else:
-                self.primary_in.T = self.primary_in.T+1.0
-                    
+        
+    def COMP(self, eff_isen: float, eff_mech: float, DSH):
+        h_comp_out_isen = PropsSI('H','P',self.primary_out.p,'S',self.primary_in.s,self.primary_in.fluidmixture)
+        self.primary_out.h = (h_comp_out_isen - self.primary_in.h)/eff_isen + self.primary_in.h
+        self.primary_out.T = PropsSI('T','H',self.primary_out.h,'P',self.primary_out.p,self.primary_in.fluidmixture)
+        self.primary_out.s = PropsSI('S','T',self.primary_out.T,'P',self.primary_out.p,self.primary_in.fluidmixture)
+        try:
+            T_comp_out_sat = PropsSI('T','P',self.primary_out.p,'Q',1.0,self.primary_in.fluidmixture)
+        except:
+            T_comp_out_sat = self.primary_out.T_crit
+        if abs(self.primary_out.T - T_comp_out_sat) < 1: # Overhanging problem protection
+            DSH = DSH + 1.0
+            cond_a = 0
+        else:
+            cond_a = 1
             
         self.Pspecific = (self.primary_out.h - self.primary_in.h)/eff_mech
-            
+        
+        return (DSH, cond_a)
+        
+        
     def EXPAND(self, eff_isen: float, eff_mech: float):
         if eff_isen > 0.0:
             h_expand_out_isen = PropsSI('H','P',self.primary_out.p,'S', self.primary_in.s, self.primary_in.fluidmixture)
